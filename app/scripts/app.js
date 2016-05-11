16d398e7cd5f39e58ecdc3c02c4f46b6957e3251
Misael Cázares <misael.upg@gmail.com>
2016-05-11 02:58:53 +0000

    Initial Commit App Scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name proFileApp
+ * @description
+ * # proFileApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('proFileApp', [
+    'ngResource',
+    'ngSanitize'
+  ]);
